a8c75b1bd08cbd127ccba4658bc4a84511809d8d
sbey90 <63749496+sbey90@users.noreply.github.com>
2020-08-27 21:18:36 -0400

    Add files via upload

---
+++ UnitConversion/Project1.js
@@ -0,0 +1,15 @@
+let fahrenheit = prompt("Please enter a number of degrees Fahrenheit to be converted.");
+let result = (78 - 32)* 5/9;
+alert ("That's "+result+" celsius");
+
+let celcius = prompt("Please enter a number of degrees Celcius to be converted to Kelvins.");
+let resultc = 25.556 + 273.15;
+alert ("That's "+resultc+" kelvins");
+
+let pound = prompt("Please enter a number of Pounds to be converted to Kilograms.");
+let resultp = 1 / 2.2046;
+alert("That's "+resultp+" kilograms");
+
+let meter = prompt("Please enter a number of Meters to be converted to Centimeters.");
+let resultm = 5*100;
+alert("That's "+resultm+" centimeters");
